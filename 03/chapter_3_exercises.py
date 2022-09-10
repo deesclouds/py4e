5fcf3e7f31f877f16cd4920f12eafe76d3614717
Darnell Davis <deescloud@protonmail.com>
2022-09-10 14:33:02 -0700

    remove float variable from string input, update program to quit before computing pay

--- 03/chapter_3_exercises.py
+++ 03/chapter_3_exercises.py
@@ -16,20 +16,26 @@ print('Pay:$' ,gross_pay)
 
 # Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by printing a message and exiting the program. The following shows two executions of the program:
 
-
-name = input('Please enter your full name: ')
+string_hours = input('Enter Hours: ')
+string_rate = input('Enter Rate: ')
 try: 
-    hours = float(input('Enter Hours: '))
-    rate = float(input('Enter Rate: '))
+    hours = float(string_hours)
+    rate = float(string_rate)
 except:
     print('Error, please enter numeric input.')
-    if hours > 40:
-        reg_pay = hours * rate 
-        over_pay = (hours - 40.0) * (rate * 0.5)
-        gross_pay = reg_pay + over_pay
-    else:
-        gross_pay = hours * rate
-        print('Pay:$' ,gross_pay)
+print(hours, rate)
+quit()
+if hours > 40:
+    # base pay
+    reg_pay = hours * rate 
+    # bonus hours with time and a half
+    over_pay = (hours - 40.0) * (rate * 0.5)
+    # gross pay totalling base pay and overtime
+    gross_pay = reg_pay + over_pay
+else:
+    gross_pay = hours * rate
+    # gross pay for regular hours
+    print('Pay:$' ,gross_pay)
 
 # Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error message. If the score is between 0.0 and 1.0, print a grade using the following table.
 
