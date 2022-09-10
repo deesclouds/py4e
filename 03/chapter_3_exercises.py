b52d5d1460a954e78ce2e7f353ad4d1ca80561d5
Darnell Davis <deescloud@protonmail.com>
2022-09-10 14:29:02 -0700

    seperate float variables from input to compute hourly pay

--- 03/chapter_3_exercises.py
+++ 03/chapter_3_exercises.py
@@ -2,12 +2,13 @@
 
 # Rewrite your pay computation to give the employee 1.5 times the hourly rate for hours worked above 40 hours.
 
-hours = float(input('Enter Hours: '))
-rate = float(input('Enter Rate: '))
+string_hours = input('Enter Hours: ')
+string_rate = input('Enter Rate: ')
+hours = float(string_hours)
+rate = float(string_rate)
 if hours > 40:
     reg_pay = hours * rate 
     over_pay = (hours - 40.0) * (rate * 0.5)
-
     gross_pay = reg_pay + over_pay
 else:
     gross_pay = hours * rate
